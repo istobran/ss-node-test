86f03dd044b47b62ca64be2b36b108b9c5fc28c2
BangZ <i@bangz.me>
2017-03-03 17:25:25 +0800

    split code to class

--- index.js
+++ index.js
@@ -10,143 +10,41 @@ import tcpp from "tcp-ping";
 import dns from "dns";
 import Table from "cli-table2";
 import Gauge from "gauge";
+import Task from "../task";
 
-// 帮助信息
-const helpMessage = `
-  usage: node index.js [your_shadowsocks_config_file]
-`;
-
-// 判断是否输出帮助信息
-if (process.argv.length != 3 || process.argv[2] == "-h" || process.argv[2] == "--help") {
-  console.log(helpMessage);
-  process.exit();
-}
-const file_path = process.argv[2];
-var config_data;
-// 判断文件是否可访问
-try {
-  if (!fs.lstatSync(file_path).isFile()) {    // 如果不是文件
-    throw new Error(`'${file_path}' is not a vaild file!`);
-  } else if (path.extname(file_path) != ".json") {    // 如果不是 json 格式
-    throw new Error(`'${file_path}' is not a json file!`);
-  } else {    // 如果是 json 格式的文件
-    config_data = jsonfile.readFileSync(file_path);
-  }
-} catch (e) {   // 文件不存在
-  throw new Error(`'${file_path}' no such file or directory!`);
-}
-
-// 表格数据部分
-const nodeList = config_data.configs;
-const pureData = new Array();
-const out = new Table({
-  head: ['Hostname', 'Resolve', 'Port', 'Delay'],
-  colWidths: [20, 20, 15]
-});
-var curr_index = 1;
-var curr_target = "";
-
-// 进度条部分
-const gt = new Gauge(process.stderr, {    // 进度条对象
-  updateInterval: 50,
-  cleanupOnExit: true
-});
-var progress = 0;   // 进度
-var pulse = setInterval(function () {   // 说明信息更新器
-  gt.pulse(`正在测试服务器 ${curr_target} 的延迟 ${curr_index}/${nodeList.length}`)
-}, 110);
-var prog = setInterval(function () {    // 进度条更新器
-  progress = curr_index / nodeList.length;
-  gt.show(Math.round(progress * 100)+"%", progress)
-  if (progress >= 1) {
-    clearInterval(prog)
-    clearInterval(pulse)
-    gt.disable()
-  }
-}, 100);
-
-// 检查是否为IPv4
-const isIPv4 = (ip) => {
-  return /^(?=\d+\.\d+\.\d+\.\d+$)(?:(?:25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]|[0-9])\.?){4}$/.test(ip);
-}
-
-// 调用DNS解析IP地址
-const resolveIP = (item) => {
-  return new Promise((resolve, reject) => {
-    if (isIPv4(item.server)) {
-      item.ip = item.server;
-      resolve();
-    } else {
-      dns.lookup(item.server, (err, addresses, family) => {
-        if (typeof addresses === 'undefined') {
-          item.ip = "无法解析";
-          // reject("无法解析");
-        } else {
-          item.ip = addresses;
-        }
-        resolve();
-      });
+class Main {
+  /**
+   * 构造方法，初始化json数据
+   */
+  constructor() {
+    this.helpMessage = `usage: node index.js [your_shadowsocks_config_file]`;   // 帮助信息
+    // 判断是否输出帮助信息
+    if (process.argv.length != 3 || process.argv[2] == "-h" || process.argv[2] == "--help") {
+      console.log(helpMessage);
+      process.exit();
     }
-  });
-}
-
-// 获取延迟
-const ping = (item) => {
-  return new Promise((resolve, reject) => {
-    curr_target = item.server;
-    resolveIP(item).then(() => {
-      if (item.ip == "无法解析") {      // 解析失败
-        out.push([item.server, "无法解析", item.server_port, "无法连接"]);
-        resolve();
-      } else {    // 解析成功
-        tcpp.ping({ address: item.ip, port: item.server_port, timeout: 500, attempts: 1 }, (err, data) => {
-          out.push([item.server, item.ip, item.server_port, data.avg ? data.avg.toFixed(3) : "无法连接"]);
-          pureData.push({
-            hostname: item.server,
-            ipaddr: item.ip,
-            port: item.server_port,
-            delay: data.avg ? data.avg.toFixed(3) : 999
-          });
-          resolve();
-        });
+    const file_path = process.argv[2];
+    this.config_data;
+    // 判断文件是否可访问
+    try {
+      if (!fs.lstatSync(file_path).isFile()) {    // 如果不是文件
+        throw new Error(`'${file_path}' is not a vaild file!`);
+      } else if (path.extname(file_path) != ".json") {    // 如果不是 json 格式
+        throw new Error(`'${file_path}' is not a json file!`);
+      } else {    // 如果是 json 格式的文件
+        this.config_data = jsonfile.readFileSync(file_path);
       }
-    })
-  });
-}
-
-const min = (data) => {
-  var min = null;
-  data.forEach((item, index) => {
-    if (index === 0) {
-      min = item;
-    } else if (item.delay < min.delay) {
-      min = item;
+    } catch (e) {   // 文件不存在
+      throw new Error(`'${file_path}' no such file or directory!`);
     }
-  });
-  return min;
-}
-
-// 主入口
-const main = () => {
-    // 返回promise对象的函数的数组
-    var tasks = [];
-    nodeList.forEach((item) => {
-      tasks.push(() => { return ping(item).then(() => {curr_index++}) });
-    });
-    var promise = Promise.resolve();
-    // 开始的地方
-    tasks.forEach(fn => {
-      promise = promise.then(fn);
-    });
-    gt.show();
-    return promise;
+  }
+  /**
+   * 执行入口
+   */
+  execute() {
+    new Task(this.config_data).run();
+  }
 }
 
 // 运行主程序
-main().then(function (value) {
-  console.log(out.toString());
-  const fastest = min(pureData);
-  console.log(`统计：共计 ${nodeList.length} 个节点，其中最快节点的是 ${fastest.hostname}(${fastest.delay})`);
-}).catch(function(error){
-  console.error(error);
-});
+new Main();
--- task.js
+++ task.js
@@ -1,3 +1,120 @@
 export default class {
-  
+  /**
+   * 构造方法，用于初始化私有变量
+   * @param {Object} config_data 配置文件数据
+   */
+  constructor(config_data) {
+    // 表格数据部分
+    this.nodeList = config_data.configs;
+    this.pureData = new Array();
+    this.out = new Table({
+      head: ['Hostname', 'Resolve', 'Port', 'Delay'],
+      colWidths: [20, 20, 15]
+    });
+    this.curr_index = 1;
+    this.curr_target = "";
+  }
+
+  /**
+   * 检查是否为IPv4
+   * @param {String} ip 要判断的IP地址
+   * @return {Boolean}
+   */
+  isIPv4(ip) => {
+    return /^(?=\d+\.\d+\.\d+\.\d+$)(?:(?:25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]|[0-9])\.?){4}$/.test(ip);
+  }
+
+  /**
+   * 调用DNS解析IP地址
+   * @param {Object} item 服务器对象
+   * @return {Promise}
+   */
+  const resolveIP = (item) => {
+    return new Promise((resolve, reject) => {
+      if (isIPv4(item.server)) {
+        item.ip = item.server;
+        resolve();
+      } else {
+        dns.lookup(item.server, (err, addresses, family) => {
+          if (typeof addresses === 'undefined') {
+            item.ip = "无法解析";
+            // reject("无法解析");
+          } else {
+            item.ip = addresses;
+          }
+          resolve();
+        });
+      }
+    });
+  }
+
+  /**
+   * 使用tcp-ping测试延迟
+   * @param {Object} item 服务器对象
+   * @return {Promise}
+   */
+  const ping = (item) => {
+    return new Promise((resolve, reject) => {
+      curr_target = item.server;
+      resolveIP(item).then(() => {
+        if (item.ip == "无法解析") {      // 解析失败
+          out.push([item.server, "无法解析", item.server_port, "无法连接"]);
+          resolve();
+        } else {    // 解析成功
+          tcpp.ping({ address: item.ip, port: item.server_port, timeout: 500, attempts: 1 }, (err, data) => {
+            out.push([item.server, item.ip, item.server_port, data.avg ? data.avg.toFixed(3) : "无法连接"]);
+            pureData.push({
+              hostname: item.server,
+              ipaddr: item.ip,
+              port: item.server_port,
+              delay: data.avg ? data.avg.toFixed(3) : 999
+            });
+            resolve();
+          });
+        }
+      })
+    });
+  }
+
+  /**
+   * 求出延迟最低节点
+   * @param {Array} data 服务器延迟列表
+   * @return {Object}
+   */
+  const min = (data) => {
+    var min = null;
+    data.forEach((item, index) => {
+      if (index === 0) {
+        min = item;
+      } else if (item.delay < min.delay) {
+        min = item;
+      }
+    });
+    return min;
+  }
+
+  /**
+   * 执行测试任务
+   */
+  run() {
+    // 返回promise对象的函数的数组
+    var tasks = [];
+    nodeList.forEach((item) => {
+      tasks.push(() => { return ping(item).then(() => {curr_index++}) });
+    });
+    var promise = Promise.resolve();
+    // 开始的地方
+    tasks.forEach(fn => {
+      promise = promise.then(fn);
+    });
+    gt.show();
+    return promise;
+    main().then(function (value) {
+      console.log(out.toString());
+      const fastest = min(pureData);
+      console.log(`统计：共计 ${nodeList.length} 个节点，其中最快节点的是 ${fastest.hostname}(${fastest.delay})`);
+    }).catch(function(error){
+      console.error(error);
+    });
+  }
 }
